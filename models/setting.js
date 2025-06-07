f04a2c59ba2d19d6cded9d4d07028b6f2cf06192
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-06-07 21:30:06 +0300

    feat(models): Added Setting model for controlling homepage content and User model for admin access

---
+++ models/setting.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose");
+const settingSchema = new mongoose.Schema({
+
+  heroSection: {
+    name: String,
+    jobTitle: String,
+    welcomeMessage: String,
+    backgroundImage: String,
+  },
+
+  aboutSection: {
+    title: String,
+    content: String,
+    profileImage: String,
+    birthday: String,
+    website: String,
+    phone: String,
+    city: String,
+    age: Number,
+    degree: String,
+    email: String,
+    freelance: String,
+  },
+
+  factsSection: [
+    {
+      title: String,
+      count: Number,
+      icon: String,
+    }
+  ],
+
+  skillsSection: [
+    {
+      name: String,
+      percentage: Number
+    }
+  ],
+
+  resumeSection: {
+    education: [
+      {
+        title: String,
+        from: String,
+        to: String,
+        description: String
+      }
+    ],
+    experience: [
+      {
+        title: String,
+        from: String,
+        to: String,
+        description: String
+      }
+    ]
+  },
+
+  servicesSection: [
+    {
+      title: String,
+      icon: String,
+      description: String
+    }
+  ],
+
+  testimonialsSection: [
+    {
+      name: String,
+      image: String,
+      content: String
+    }
+  ],
+
+  contactSection: {
+    location: String,
+    email: String,
+    phone: String,
+    mapEmbed: String
+  },
+
+  socialLinks: {
+    github: String,
+    linkedin: String,
+    twitter: String,
+    facebook: String,
+    instagram: String,
+    youtube: String,
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Setting", settingSchema);
