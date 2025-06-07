3fe6a950386db1b2c44a6424d86519e345a15cde
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-06-07 14:46:02 +0300

    Integrated SB Admin2 dashboard template into the project and split the layout into reusable EJS partials for better maintainability

--- routes/coreRoute.js
+++ routes/coreRoute.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('index', { title: 'Express' });
 });
+router.get("/dashboard", (req, res) => {
+    res.render('dashboard', { title: 'Dashboard' });
+});
 
 module.exports = router;
